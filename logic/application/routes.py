46415cbfebb6e20eec9a77a67efcf14afd2f442c
Ubuntu <fergus@swarm-mster.l32j2sayrp2ediazujtsnzkuwa.zx.internal.cloudapp.net>
2020-04-05 13:51:24 +0000

    Fixes

--- logic/application/routes.py
+++ logic/application/routes.py
@@ -8,8 +8,8 @@ import random
 @app.route('/logic', methods=['GET', 'POST'])
 def post_numbers():
     #accountNumber = requests.get('http://51.132.128.111:5000/number') + requests.get('http://51.132.128.111:5000/letter')
-    api = 'http://51.104.223.47:5003/number'
-    api2 = 'http://51.104.223.47:5002/letter'
+    api = 'http://number:5003/get'
+    api2 = 'http://letter:5002/get'
     response = requests.get(api)
     response1 = requests.get(api2)
     accountNumber = str(response.text) + str(response1.text)
