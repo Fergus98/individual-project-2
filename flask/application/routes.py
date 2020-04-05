093734644be718df37290225231fc98a61d11a7c
Ubuntu <fergus@swarm-mster.l32j2sayrp2ediazujtsnzkuwa.zx.internal.cloudapp.net>
2020-04-05 14:55:50 +0000

    Fixes

--- flask/application/routes.py
+++ flask/application/routes.py
@@ -6,7 +6,7 @@ import requests
 
 @app.route('/home', methods=['GET'])
 def post_numbers():
-    api3 = 'http://logic:5001/get'
+    api3 = 'http://logic:5001/logic'
     response3 = requests.get(api3)
     response4 = response3.text
     return render_template('home.html', title='Home', response=response4)
--- logic/application/routes.py
+++ logic/application/routes.py
@@ -8,8 +8,8 @@ import random
 @app.route('/logic', methods=['GET', 'POST'])
 def post_numbers():
     #accountNumber = requests.get('http://51.132.128.111:5000/number') + requests.get('http://51.132.128.111:5000/letter')
-    api = 'http://number:5003/get'
-    api2 = 'http://letter:5002/get'
+    api = 'http://numbers:5003/number'
+    api2 = 'http://letters:5002/letter'
     response = requests.get(api)
     response1 = requests.get(api2)
     accountNumber = str(response.text) + str(response1.text)
